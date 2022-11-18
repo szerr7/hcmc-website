d4262f463cd635d196ba62823d9cf59e5783e5a4
samuel <94730955+sameharu@users.noreply.github.com>
2022-11-18 00:17:50 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+/**
+ * app.js
+ */
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+
+          const registration = document.querySelector("#registration");
+
+          registration.addEventListener("submit", (event) => {
+               
+                event.preventDefault();
+                
+                const txtPatientId = document.querySelector("#patientIdNumber") ;
+
+                const txtFirstName = document.querySelector("#firstName") ;
+
+                const txtMiddleName = document.querySelector("#middleInitials") ;
+
+                const txtLastName = document.querySelector("#lastName") ;
+
+                const txtDateOfBirth = document.querySelector("#dateOfBirth") ;
+
+                const txtDepartment = document.querySelector("#ddlDepartment") ;
+
+                const txtOutPatient = document.querySelector("input[name='radioIsOutPatient']:checked") ;
+
+                
+
+
+                 
+
+
+               
+
+
+          });
+
+
+
+          const newAccountTabe = function()
+
+
+
+
+});
